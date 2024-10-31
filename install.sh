8cedd818c00d1cccd6bb9b490833cf85112f600b
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-10-31 16:28:44 +0900

    Update dotfiles

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 su $USER -c "git clone https://github.com/MikiyaShibuya/dotfiles.git /tmp/dotfiles"
 cd /tmp/dotfiles
-su $USER -c "git checkout lua"
+su $USER -c "git checkout b7437e8"
 
 ./install.sh
 
