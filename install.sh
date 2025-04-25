1545032df5987f1c427cc00367b977df61420131
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2025-04-25 14:48:51 +0900

    Update dotfiles to 4d6d902

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ REPO_URL=https://github.com/MikiyaShibuya/dotfiles.git
 su $USER -c "mkdir -p $HOMEDIR/.local/share"
 su $USER -c "git clone $REPO_URL --depth=1 $HOMEDIR/.local/share/dotfiles"
 cd $HOMEDIR/.local/share/dotfiles
-su $USER -c "git checkout f41996d"
+su $USER -c "git checkout 4d6d902"
 
 USER=$USER ./install.sh
 
